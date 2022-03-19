c4fd16b1a7f133e9652b6a60fff897d856ada026
Rutte <100002298+ruttesilva@users.noreply.github.com>
2022-03-18 22:52:42 -0300

    Finaliza o exercício

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿double n1, n2, n3, media;
+
+Console.WriteLine("Média Aritmética de 3 números\n");
+
+Console.Write("Digite o primeiro número..: ");
+n1= Convert.ToDouble(Console.ReadLine()!);
+
+Console.Write("Digite o segundo número...: ");
+n2= Convert.ToDouble(Console.ReadLine()!);
+
+Console.Write("Digite o terceiro número..: ");
+n3= Convert.ToDouble(Console.ReadLine()!);
+
+//dividir pela quantidade de números
+media = (n1 + n2 + n3) / 3; 
+
+Console.WriteLine($"Média: {media:N1}");
